8c72c1ee4597f1b573b0775f38249a8bafdba605
hhudsonjr <hhudsonjr@gmail.com>
2017-08-10 17:08:21 -0400

    Add files via upload

---
+++ Apple Orange.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        string[] tokens_s = Console.ReadLine().Split(' ');
+        int s = Convert.ToInt32(tokens_s[0]);
+        int t = Convert.ToInt32(tokens_s[1]);
+        string[] tokens_a = Console.ReadLine().Split(' ');
+        int a = Convert.ToInt32(tokens_a[0]);
+        int b = Convert.ToInt32(tokens_a[1]);
+        string[] tokens_m = Console.ReadLine().Split(' ');
+        int m = Convert.ToInt32(tokens_m[0]);
+        int n = Convert.ToInt32(tokens_m[1]);
+        string[] apple_temp = Console.ReadLine().Split(' ');
+        int[] apple = Array.ConvertAll(apple_temp,Int32.Parse);
+        string[] orange_temp = Console.ReadLine().Split(' ');
+        int[] orange = Array.ConvertAll(orange_temp,Int32.Parse);
+        
+        foreach(int i in apple) {
+          if((i + a) < s || (i + a) > t) {
+            m--;
+            }
+          }
+        foreach(int j in orange) {
+          if((j + b) > t || (j + b) < s) {
+            n--;
+            }
+          }
+          Console.WriteLine(m);
+          Console.WriteLine(n);
+    }
+}
